06957b0d231089d78d01407875731c49ced214a0
Alperen Remzi Ünal <alperen061706@gmail.com>
2022-06-05 02:54:37 +0300

    Update gatsby-config.js

--- alperenremziunalportfolio/gatsby-config.js
+++ alperenremziunalportfolio/gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://alperenremziunal.me/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
           siteName: "Alperen Remzi Ünal", // Used in manifest.json
