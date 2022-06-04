b8b07e8f01e8aa965f34e22246fc2067dc5805ba
Alperen Remzi Ünal <alperen061706@gmail.com>
2022-06-04 23:40:59 +0300

    initial commit

---
+++ alperenremziunalportfolio/content/sections/about/about.md
@@ -0,0 +1,8 @@
+---
+imageSrc: "../../images/charles-deluvio-DgoyKNgPiFQ-unsplash.jpg"
+imageAlt: "About Alperen Remzi Ünal"
+---
+
+Computer science student who can think outside of the box, is results-oriented, outspoken, analytical and aims to be a software engineer.
+
+Photo by <a href="https://unsplash.com/@charlesdeluvio?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="nofollow noopener noreferrer" aria-label="External Link"><u>Charles Deluvio</u></a> on Unsplash
---
+++ alperenremziunalportfolio/src/pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  AboutSection,
+  ArticlesSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Alperen Remzi Ünal" />
+      <Page useSplashScreenAnimation>
+        <HeroSection sectionId="hero" />
+        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <AboutSection sectionId="about" heading="About Me" />
+        <InterestsSection sectionId="details" heading="Interests & Skills" />
+        <ProjectsSection sectionId="features" heading="Projects" />
+        <ContactSection sectionId="github" heading="GitHub" />
+      </Page>
+    </>
+  );
+}
